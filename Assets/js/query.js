7578e5d834d67f24233da182ad25e776bb06e1d9
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-10-13 19:58:38 -0400

    write comparaison time function

--- Assets/js/query.js
+++ Assets/js/query.js
@@ -3,37 +3,47 @@ let today=moment();
 let today_Date_Hour = today.format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(today_Date_Hour);
 
-
-
 const grandParent=$('.container');
 
+
+/*Create Timesblock  with data-number*/
+function createTimeBlock(){
 for (let i=0;i<=10;i++){
 
-    const StartDateWork = moment("08:00","hh:00");
-    const dataHour = moment("8","h");
+    const StartDateWork = moment("10:00","hh:00");
+    const dataHour = moment("10","h");
+  
+
+  let f =  dataHour.add(i, 'hours').format('h');
 
 
-    let parentDiv= $('<div>');
+    const parentDiv= $('<div>');
     const childSpan =$('<span>');
-    let childInput = $('<input>');
-    let childButton =$('<button>');
+    const childInput = $('<input>');
+    const childButton =$('<button>');
 
     parentDiv.attr('class', 'input-group-append');
+    parentDiv.attr('data-number',f);
 
-    childSpan.attr('class','input-group-text Hour');
+    childSpan.attr('class','input-group-text');
+
     childSpan.attr('id','basic-addon2');
-
-
     childSpan.text(StartDateWork.add(i, 'hours').format('hh:00 A'));
+    childSpan.addClass('s'+f);
+    
 
     childInput.attr('type','text');
+    //.addClass();
     childInput.attr('class','form-control');
+
+    childInput.attr('id','f'+f);
+    f++;
     childInput.attr('placeholder','Task disponable');
     childInput.attr('aria-label','Task disponable');
     childInput.attr('aria-describedby','basic-addon2');
-    childInput.attr('data-number',dataHour.add(i, 'hours').format('h'));
 
-    childInput.attr('type','button');
+
+    childButton.attr('type','button');
     childButton.attr('class','btn btn-outline-secondary');
     childButton.attr('id','button-addon2');
     childButton.text('Save');
@@ -43,5 +53,78 @@ for (let i=0;i<=10;i++){
     parentDiv.append(childButton);
     grandParent.append(parentDiv);
 
+
 }
+}
+createTimeBlock();
+
+
+
+/** Compaarison of Time */
+
+
+function IntervalTime(){
+
+
+    const currentHour = moment().startOf('hour')
+    //console.log(currentHour);
+
+   for (let i=0;i<=10;i++){
+
+          const dataHour = moment("10","h");
+
+          let  f =  dataHour.add(i, 'hours').format('h');
+     
+
+
+          let strTime = $(".s"+f).text();
+          let timeMoment = moment(strTime,'hh:00 A');
+
+
+
+              if (currentHour.isAfter(timeMoment)) {
+
+        
+              $('#f'+f).css({ 'background-color': 'gray', 'color': 'black'}); 
+          }
+
+              else if (currentHour.isBefore(timeMoment)) {
+      
+              $('#f'+f).css({ 'background-color': 'yellow', 'color': 'white' });
+          }
+
+
+              else if (currentHour.isSame(timeMoment)) {
+
+            $('#f'+f).css({ 'background-color': 'green', 'color': 'white'}); 
+        
+              }
+
+
+
+
+    let hourTitle =moment.unix(timeMoment/1000).format("hh:00 A");
+
+
+    console.log("-------moment () = "+currentHour+" and moment (text)------->"+timeMoment);
+    console.log("********************"+moment.unix(currentHour/1000).format("hh:00 A")+" ..........."+hourTitle+" ///////////////// "); 
+    console.log("-------AFTER------->"+currentHour.isAfter(timeMoment));
+
+    console.log("--------BEFOR------>"+currentHour.isBefore(timeMoment));  
+
+    console.log("--------BEFOR------>"+currentHour.isSame(timeMoment));  
+
+
+
+
+    
+    }
+
+  }
+
+
+
+IntervalTime();
+
+
 
