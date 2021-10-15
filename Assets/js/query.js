c0f7750c8028c7b1604d3019ff2a060e9e1dd4ef
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-10-14 23:59:24 -0400

    change timeblocks to a standard business hour and add a clear button to delete all data with the key from the local storage and update the save button with an icon

--- Assets/js/query.js
+++ Assets/js/query.js
@@ -7,11 +7,14 @@ const grandParent=$('.container');
 
 
 /*Create Timesblock  with data-number*/
-function createTimeBlock(){
-for (let i=0;i<12;i++){
 
-    const StartDateWork = moment("08:00","hh:00");
-    const dataHour = moment("8","h");
+// standard business hours are 9:00 a.m. to 5:00 p.m.,
+
+function createTimeBlock(){
+for (let i=0;i<=8;i++){
+
+    const StartDateWork = moment("09:00","hh:00");
+    const dataHour = moment("9","h");
   
 
   let f =  dataHour.add(i, 'hours').format('h');
@@ -21,24 +24,21 @@ for (let i=0;i<12;i++){
     const childSpan =$('<span>');
     const childInput = $('<textarea>');
     const childButton =$('<button>');
+    const saveIcon =$('<i>');
 
     parentDiv.attr('class', 'input-group-append');
     parentDiv.attr('data-number',f);
 
     childSpan.attr('class','input-group-text');
-
     childSpan.attr('id','basic-addon2');
     childSpan.text(StartDateWork.add(i, 'hours').format('hh:00 A'));
     childSpan.addClass('s'+f);
     
 
     childInput.attr('type','text');
-    //.addClass();
-    childInput.attr('class','form-control');
-
+    childInput.addClass('form-control');
     childInput.attr('id','f'+f);
     f++;
-    //childInput.attr('placeholder','Task disponable');
     childInput.attr('aria-label','With textarea');
     childInput.attr('aria-describedby','basic-addon2');
 
@@ -47,11 +47,13 @@ for (let i=0;i<12;i++){
     childButton.attr('class','btn btn-outline-secondary');
     childButton.addClass("btn"+f);
     childButton.attr('id','button-addon2');
-    childButton.text('Save');
+    saveIcon.addClass('fas fa-cloud-download-alt');
+
 
     parentDiv.append(childSpan);
     parentDiv.append(childInput);
     parentDiv.append(childButton);
+    childButton.append(saveIcon);
     grandParent.append(parentDiv);
 
 
@@ -70,9 +72,9 @@ function IntervalTime(){
     const currentHour = moment().startOf('hour');
 
 
-    for (let i=0;i<12;i++){
+    for (let i=0;i<=8;i++){
 
-          const dataHour = moment("8","h");
+          const dataHour = moment("9","h");
 
           let  f =  dataHour.add(i, 'hours').format('h');
      
@@ -132,25 +134,21 @@ IntervalTime();
 function RetreiveAllTasks(){
 
 
- // getItem
+ // getItem from The LocalStorage
   
   let allTasksInput = localStorage.getItem("newTask");
 
   if(allTasksInput === null){
       allTasksInput = [];
   }else{
-    allTasksInput = JSON.parse(allTasksInput);
+      allTasksInput = JSON.parse(allTasksInput);
    
-  
     for (let i = 0; i < allTasksInput.length; i++) {
   
-    $("#f"+allTasksInput[i].parent).val(allTasksInput[i].textInput);
+       $("#f"+allTasksInput[i].parent).val(allTasksInput[i].textInput);
   
   
     }
-  
-  
-  
   }
 
   return allTasksInput;
@@ -190,7 +188,7 @@ let dataTask={
 
 };
 
-//setItem 
+//setItem  to The LocalStorage
 
 SetTasks(tab,dataTask);
 
@@ -210,3 +208,33 @@ let newTask = JSON.stringify(allTasks);
 localStorage.setItem("newTask", newTask);
 location.reload();
 }
+
+
+
+// Delete Button to all Data with key "newTask" from the LocaleStorage
+
+
+
+
+const childClearButton =$('<button>');
+const trashIcon =$('<i>');
+
+
+childClearButton.attr('type','button');
+childClearButton.addClass("btn btn-lg btn-block btn-outline-danger");
+childClearButton.attr('id','clear');
+
+trashIcon.addClass("fas fa-trash-alt");
+// childClearButton.text('Clear');
+grandParent.append(childClearButton);
+childClearButton.append(trashIcon);
+
+
+$('#clear').on('click',function(event){
+
+  event.preventDefault();
+
+  localStorage.removeItem("newTask");
+  location.reload();
+
+});
